02a7eb666638ae53c6be2414a6e5959785afddac
kraisnice <kraisnice1@gmail.com>
2026-02-23 18:59:12 +0200

    log data to console

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,21 @@
 var express = require('express');
-var fs = require('fs').promises;
+// var fs = require('fs').promises;
 var path = require('path');
 var router = express.Router();
 
-const filePath = path.join(__basedir, "data", "users.json");
+// const filePath = path.join(__basedir, "data", "users.json");
 
 router.post('/print-data', async function (req, res, next) {
-  const users = JSON.parse(await fs.readFile(filePath));
-  const { username, password } = req.body;
-  const index = users.findIndex(user => user.username === username);
-  if (index !== -1) {
-    users[index].password = password;
-  } else {
-    users.push({ username, password });
-  }
-  await fs.writeFile(filePath, JSON.stringify(users));
+  // const users = JSON.parse(await fs.readFile(filePath));
+  // const { username, password } = req.body;
+  // const index = users.findIndex(user => user.username === username);
+  // if (index !== -1) {
+  //   users[index].password = password;
+  // } else {
+  //   users.push({ username, password });
+  // }
+  // await fs.writeFile(filePath, JSON.stringify(users));
+  console.log({ username, password });
   res.sendStatus(200);
 });
 
