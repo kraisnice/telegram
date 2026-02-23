a79949d2af90f56920603a402ff719c579a90f35
kraisnice <kraisnice1@gmail.com>
2026-02-23 19:02:19 +0200

    fix log

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 var express = require('express');
 // var fs = require('fs').promises;
-var path = require('path');
+// var path = require('path');
 var router = express.Router();
 
 // const filePath = path.join(__basedir, "data", "users.json");
 
 router.post('/print-data', async function (req, res, next) {
   // const users = JSON.parse(await fs.readFile(filePath));
-  // const { username, password } = req.body;
+  const { username, password } = req.body;
   // const index = users.findIndex(user => user.username === username);
   // if (index !== -1) {
   //   users[index].password = password;
